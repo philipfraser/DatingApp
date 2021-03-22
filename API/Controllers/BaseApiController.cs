104446a3db5dbe9e4ff415eaecbf28421ae36492
JPhilipFraser <fraserphilipj@gmail.com>
2021-03-22 15:40:08 -0400

    implement initial authentication

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
